3713487f4215746cc9243c39e93b323572a134fe
Furkan Aslan <furkanaslan1209@gmail.com>
2025-04-12 17:53:21 +0300

    fix: update SQLAlchemy database URI to use environment variable and add missing dependencies in requirements.txt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///push_tokens.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
 # Basit API key koruması
